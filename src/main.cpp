f4d9d8f4c551612e8498f93171af869b030a8e15
Vincent <147661349+Skzter@users.noreply.github.com>
2025-05-13 02:46:35 +0200

    aufgeräumt und dreieck für resultate ordner gerendert

--- src/main.cpp
+++ src/main.cpp
@@ -6,51 +6,15 @@ int main()
 {
     Scene test;
     std::ifstream file;
-    file.open("cow.ply",std::ios::in);
+    std::string filePath = "models/triangle.ply";
+    file.open(filePath,std::ios::in);
     if(!file.is_open()){
         std::cout << "File not opened" << std::endl;
+        return -1;
     }
     test.loadFile(file);
-    test.camera = Camera(Vector3D(0,2, -5), Vector3D(0,0,1), 1.0, 1.0, 316, 316);
+    test.camera = Camera(Vector3D(0,0, -2), Vector3D(0,0,1), 1.5, 1.5, 40, 40);
     test.drawPicture();
 
-    //Vector3D vec1 = Vector3D(0,1,1), vec2 = Vector3D(0,1,0);
-    //std::cout << Vector3D::dot(vec1, vec2) << std::endl;
-    /*
-    Point3D p;
-    p.setPoint(2,3,5);
-    p.printPoint();
-
-    Point3D p2;
-    p2.setPoint(1,1,1);
-    p2.printPoint();
-
-    p2.addPoint(p);
-    p2.printPoint();
-
-    Point3D p1 = Point3D(7,8,9);
-    p1.printPoint();
-
-    Point3D p3 = p + p2;
-    p3.printPoint();
-
-    p3 = p3 + p3;
-    p3.printPoint();
-    std::cout << p3 << std::endl;
-    */
-
-    /*
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    window = SDL_CreateWindow("Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, 0);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    bool running = true;
-
-    while(running)
-    {
-	if(SDL_QuitRequested()){ running = false; break; }
-    }
-
-    */
     return 0;
 }
