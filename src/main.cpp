3fc678ec1bc08c26290d9d31ec1ffbc3c3d03fd0
Vincent <147661349+Skzter@users.noreply.github.com>
2025-05-13 12:23:10 +0200

    time tracking plus neue model

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <fstream>
 #include <iostream>
+#include <chrono>
 #include "../include/Scene.hpp"
 
 int main()
 {
     Scene test;
     std::ifstream file;
-    std::string filePath = "models/triangle.ply";
+    std::string filePath = "models/tommygun.ply";
     file.open(filePath,std::ios::in);
     if(!file.is_open()){
         std::cout << "File not opened" << std::endl;
@@ -14,7 +15,11 @@ int main()
     }
     test.loadFile(file);
     test.camera = Camera(Vector3D(0,0, -2), Vector3D(0,0,1), 1.5, 1.5, 40, 40);
+    auto start = std::chrono::high_resolution_clock::now();
     test.drawPicture();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " seconds." << std::endl;
 
     return 0;
 }
