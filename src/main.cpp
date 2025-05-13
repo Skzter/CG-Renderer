d8b5556197f5b8fe3f0823db2ec059d502589d62
Vincent <147661349+Skzter@users.noreply.github.com>
2025-05-13 13:30:24 +0200

    bessere zeitzählung und dreieck in 1920x1080

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,29 @@
 int main()
 {
     Scene test;
+    /*
+    Hitpoint defHP = Hitpoint();
+    Hitpoint anHP;
+    
+    std::cout << "defHP - Dist: " << defHP.distance << " Face: " << defHP.face << " Pos: " << defHP.position << std::endl;
+    std::cout << "anHP - Dist: " << anHP.distance << " Face: " << anHP.face << " Pos: " << anHP.position << std::endl;
+    */
     std::ifstream file;
-    std::string filePath = "models/tommygun.ply";
+    std::string filePath = "models/triangle.ply";
     file.open(filePath,std::ios::in);
     if(!file.is_open()){
         std::cout << "File not opened" << std::endl;
         return -1;
     }
     test.loadFile(file);
-    test.camera = Camera(Vector3D(0.7,0, -3), Vector3D(0,0,1), 1.75, 0.875, 200, 100);
+    test.camera = Camera(Vector3D(0,0, -5), Vector3D(0,0,1), 1.0, 0.5625, 1920, 1080);
     auto start = std::chrono::high_resolution_clock::now();
     test.drawPicture();
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
-    std::cout << "Execution time: " << duration.count() << " seconds." << std::endl;
+    auto mins = std::chrono::duration_cast<std::chrono::minutes>(duration);
+    double secs = (duration - mins).count();
+    std::cout << "Execution time: " << mins.count() << "mins " << secs << "secs." << std::endl;
 
     return 0;
 }
